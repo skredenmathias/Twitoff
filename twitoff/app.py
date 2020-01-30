0dbfa099de74382f7bb946ef0390628da6796e38
skredenmathias <skredenmathias@users.noreply.github.com>
2020-01-31 00:02:02 +0100

    added postgres database

--- twitoff/app.py
+++ twitoff/app.py
@@ -52,7 +52,7 @@ from .twitter import add_or_update_user, update_all_users
 def create_app():
     """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('HEROKU_POSTGRESQL_COBALT_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
